41d42e5cd0628686b9baa80fbea79c9fdf8fea18
infan11 <infanjiounrahman20606@gmail.com>
2025-01-13 19:12:26 +0600

    added Routes.jsx

--- src/Components/Routes/Routes/Routes.jsx
+++ src/Components/Routes/Routes/Routes.jsx
@@ -2,10 +2,44 @@ import {
     createBrowserRouter,
 
 } from "react-router-dom";
+import Main from "../../Main/Main";
+import Home from "../../Home/Home/Home";
+import Colleges from "../../Colleges/Colleges/Colleges";
+import Admission from "../../Admission/Admission/Admission";
+import Profile from "../../Profile/Profile/Profile";
+import Login from "../../Authentication/Login/Login";
+import Register from "../../Authentication/Register/Register";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        // TO DO : Error Element use
+        element: <Main></Main> , 
+        children : [
+            {
+                path : "/",
+                element : <Home/>
+            },
+            {
+                path : "/colleges",
+                element : <Colleges/>
+            },
+            {
+                path : "/admission",
+                element : <Admission/>
+            },
+            {
+                path : "Profile", 
+                element : <Profile/>
+            },
+            {
+                path : "/login",
+                element : <Login/>
+            },
+            {
+                path : "/register",
+                element : <Register/>
+            }
+        ]
     },
 ]);
